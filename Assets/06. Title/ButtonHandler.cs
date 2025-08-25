58c7fc4e33d8035492ea02d4238174452c9e0df0
Cho Sang Jun <fpdltm456@gmail.com>
2025-08-25 17:25:06 +0900

    titlescene 제작중1

---
+++ Assets/06. Title/ButtonHandler.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void OnButton1Click()
+    {
+        Debug.Log("Button 1 눌림!");
+    }
+
+    public void OnButton2Click()
+    {
+        Debug.Log("Button 2 눌림!");
+    }
+
+    public void OnButton3Click()
+    {
+        Debug.Log("Button 3 눌림!");
+    }
+
+}
---
+++ Assets/06. Title/TitleButtonMesh.cs
@@ -0,0 +1,292 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class TitleButtonMesh : MonoBehaviour
+{
+    private Renderer rend;
+    private bool isMouseDownOnMe = false;
+    public InputAction mouseClickAction; // Inspector에서 할당
+
+    private Color originalColor;
+    [SerializeField] private Color ClickedColor = Color.red;
+    [SerializeField] private Color MouseDownColor = Color.yellow;
+    private bool isMouseOver = false;
+    [SerializeField] private string buttonName;
+    [SerializeField] private string buttonHaviorName;
+
+
+    private bool isRotating = false;
+    private bool isFixed90 = false;
+    private bool isBeforeClicked = false;
+
+    private float yTarget = 0f;
+    private float zLerpSpeed = 5f;
+
+
+    [SerializeField] private GameObject meshLabel; // 텍스트 오브젝트(자식으로 미리 만들어두기)
+    [SerializeField] private Vector3 labelOffset = new Vector3(0.5f, 0, 0); // 옆에 띄울 위치
+
+    
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        if (rend != null)
+        {
+            originalColor = rend.material.color;
+        }
+        if (buttonName == "TurtleTitle")
+            isRotating = true;
+    }
+
+    private void OnEnable()
+    {
+        if (mouseClickAction != null)
+        {
+            mouseClickAction.performed += OnMouseAction;
+            mouseClickAction.Enable();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (mouseClickAction != null)
+        {
+            mouseClickAction.performed -= OnMouseAction;
+            mouseClickAction.Disable();
+        }
+    }
+
+    private void OnMouseAction(InputAction.CallbackContext ctx)
+    {
+        float value = ctx.ReadValue<float>();
+        if (value > 0.5f)
+        {
+            // Mouse Down
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+                isMouseDownOnMe = (hit.transform == this.transform);
+            else
+                isMouseDownOnMe = false;
+        }
+        else
+        {
+            // Mouse Up
+            if (isMouseDownOnMe)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform == this.transform)
+                    {
+                        // 클릭 토글
+                        isBeforeClicked = !isBeforeClicked;
+
+                        // 색상 변경
+                        if (rend != null)
+                        {
+                            if (isBeforeClicked)
+                                rend.material.color = ClickedColor;
+                            else
+                                rend.material.color = originalColor;
+                        }
+
+                        // 자식 토글
+                        if (!isBeforeClicked)
+                            DestroyChildButtons();
+                        else
+                            SpawnChildButtons();
+
+                        // TurtleTitle 클릭 시 90도 회전 고정/해제
+                        if (buttonName == "TurtleTitle" || buttonName == "FoxTitle" || buttonName == "QuitTitle")
+                        {
+                            if (isBeforeClicked)
+                            {
+                                isRotating = false;
+                                isFixed90 = true;
+                            }
+                            else
+                            {
+                                isRotating = true;
+                                isFixed90 = false;
+                            }
+
+                            // buttonHaviorName에 따라 Scene 이동
+                            if (buttonHaviorName == "1vs1")
+                            {
+                                SceneManager.LoadScene("1vs1");
+                            }
+                            else if (buttonHaviorName == "1vsCPU")
+                            {
+                                SceneManager.LoadScene("1vsCPU");
+                            }
+                        }
+                        
+                    }
+                }
+                isMouseDownOnMe = false;
+            }
+        }
+    }
+    
+    private void ShowLabel()
+    {
+        if (meshLabel != null)
+        {
+            meshLabel.SetActive(true);
+            // meshLabel.transform.position = transform.position + labelOffset;
+            // 필요하다면 텍스트 내용도 변경 가능
+            // meshLabel.GetComponent<TextMeshPro>().text = "원하는 텍스트";
+        }
+    }
+
+    private void HideLabel()
+    {
+        if (meshLabel != null)
+            meshLabel.SetActive(false);
+    }
+
+
+
+    protected virtual void SpawnChildButtons()
+    {
+        // 이 오브젝트의 모든 자식 오브젝트를 활성화하고, 위치 고정
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i).gameObject;
+            child.SetActive(true);
+        }
+    }
+
+    private void DestroyChildButtons()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i).gameObject;
+            child.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        // 마우스 위치에서 Raycast
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        bool nowOver = false;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform == this.transform)
+                nowOver = true;
+        }
+
+        // TurtleTitle 자전/정지/고정 처리
+        if (buttonName == "TurtleTitle")
+        {
+            if (!isFixed90 || !isBeforeClicked)
+            {
+                if (nowOver)
+                {
+                    isRotating = false;
+                    yTarget = 0f;
+                }
+                else
+                {
+                    isRotating = true;
+                }
+            }
+
+            if (isRotating)
+            {
+                transform.Rotate(Vector3.up, 60f * Time.deltaTime, Space.World);
+            }
+
+            Vector3 euler = transform.eulerAngles;
+            if (nowOver)
+                euler.y = Mathf.LerpAngle(euler.y, yTarget, Time.deltaTime * zLerpSpeed);
+            transform.eulerAngles = euler;
+        }
+        else if (buttonName == "FoxTitle")
+        {
+            if (!isFixed90 || !isBeforeClicked)
+            {
+                if (nowOver)
+                {
+                    isRotating = false;
+                    yTarget = 90f;
+                }
+                else
+                {
+                    isRotating = true;
+                }
+            }
+
+            if (isRotating)
+            {
+                transform.Rotate(Vector3.up, 120f * Time.deltaTime, Space.World);
+            }
+
+            Vector3 euler = transform.eulerAngles;
+            if (nowOver)
+                euler.y = Mathf.LerpAngle(euler.y, yTarget, Time.deltaTime * zLerpSpeed);
+            transform.eulerAngles = euler;
+        }
+        else if (buttonName == "QuitTitle")
+        {
+            if (!isFixed90 || !isBeforeClicked)
+            {
+                if (nowOver)
+                {
+                    isRotating = false;
+                    yTarget = 0f;
+                }
+                else
+                {
+                    isRotating = true;
+                }
+            }
+
+            if (isRotating)
+            {
+                transform.Rotate(Vector3.up, 60f * Time.deltaTime, Space.World);
+            }
+
+            Vector3 euler = transform.eulerAngles;
+            if (nowOver)
+                euler.y = Mathf.LerpAngle(euler.y, yTarget, Time.deltaTime * zLerpSpeed);
+            transform.eulerAngles = euler;
+        }
+
+        // 색상 상태 관리
+        if (nowOver && !isMouseOver)
+        {
+            // 마우스 오버 시작
+            if (rend != null)
+            {
+                if (isBeforeClicked)
+                    rend.material.color = MouseDownColor; // 클릭된 상태에서 오버
+                else
+                    rend.material.color = MouseDownColor; // 클릭 안된 상태에서 오버
+            }
+            ShowLabel();
+        }
+        else if (!nowOver && isMouseOver)
+        {
+            // 마우스 오버 해제
+            if (rend != null)
+            {
+                if (isBeforeClicked)
+                    rend.material.color = ClickedColor; // 클릭된 상태로 복귀
+                else
+                    rend.material.color = originalColor; // 원래 색으로 복귀
+            }
+            HideLabel();
+        }
+        isMouseOver = nowOver;
+
+    }
+
+    
+}
